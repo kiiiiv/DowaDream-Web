0baf3d089c91bfd8bdc17827881b9762e048bfbc
kiiiiv <lizzy2@naver.com>
2023-08-04 22:46:36 +0900

    [FEAT] 상단 배너와 캐러셀제작 아직 태그부분은 디자인 안나옴

---
+++ src/components/Carousel.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import pic from '../봉사.jpg';
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+      <div className="main-bg" style={{ backgroundImage : 'url(' + pic + ')' }}></div>
+        <Carousel.Caption>
+            <p>1</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div className="main-bg" style={{ backgroundImage : 'url(' + pic + ')' }}></div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div className="main-bg" style={{ backgroundImage : 'url(' + pic + ')' }}></div>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div className="main-bg" style={{ backgroundImage : 'url(' + pic + ')' }}></div>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div className="main-bg" style={{ backgroundImage : 'url(' + pic + ')' }}></div>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
---
+++ src/components/LoginButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonPic from '../assets/로그인버튼2.png';
+
+function LoginButton(props) {
+  const { backgroundImg, onClick } = props;
+
+  return (
+    <Button
+      variant="outline-light"
+      style={{
+        backgroundImage: `url(${ButtonPic})`,
+        backgroundRepeat: 'no-repeat',
+        width: '330px',
+        height: '40px',
+        border: 'none',
+      }}
+      onClick={onClick} // onClick 이벤트 핸들러를 props로 받아옴
+    ></Button>
+  );
+}
+
+export default LoginButton;
