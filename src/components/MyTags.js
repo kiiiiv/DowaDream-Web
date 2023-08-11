bd5770964eea3abbc94635ec7f6dab58e709b41f
kiiiiv <lizzy2@naver.com>
2023-08-12 01:34:00 +0900

    [FEAT]마이페이지 구현

--- src/components/MyTags.js
+++ src/components/MyTags.js
@@ -1,18 +1,8 @@
+import { Wrapper } from "../styles/Common";
+
 function MyTags(){
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-          </div>
-          <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button> 
-          </div>
-        </div>
-      </div> 
+      <Wrapper></Wrapper>
     );
   }
   
