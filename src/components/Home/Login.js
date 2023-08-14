653f2c0344aeb7baf759242ed8abe91158b48a02
kiiiiv <lizzy2@naver.com>
2023-08-14 21:45:14 +0900

    Update Login.js

--- src/components/Home/Login.js
+++ src/components/Home/Login.js
@@ -1,9 +1,9 @@
-import useGoogleLogin from "@react-oauth/google";
+import {useGoogleLogin} from "@react-oauth/google";
 import {useCallback} from "react";
 
 const Login = ({setUserInfo, setIsLogin}) => {
 
-    const googleLogin = useCallback((response) => {
+    const onSuccess = useCallback((response) => {
         const userInfo = {
             profileImg: response.profileObj.imageUrl,
             email: response.profileObj.email,
@@ -13,13 +13,20 @@ const Login = ({setUserInfo, setIsLogin}) => {
         setIsLogin(true);
     }, [])
 
+    const {signIn} = useGoogleLogin({
+        clientId: "263882411657-kanoto1m439tbjmhahgc0hgv9u58t451.apps.googleusercontent.com",
+        onSuccess,
+        onFailure: (response) => {console.log(response)},
+        scope: 'profile email',
+        longtitle: true,
+        cookiePolicy: 'single_host_origin',
+        buttonText: 'Login'
+    })
+
     return (
-        <useGoogleLogin
-            clientId="263882411657-kanoto1m439tbjmhahgc0hgv9u58t451.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={googleLogin}
-            onFailure={(res) => console.log(res)}
-            cookiePolicy={'single_host_origin'}/>
+        <button onClick={signIn}>
+            Login
+        </button>
     );
 }
 export default Login;
