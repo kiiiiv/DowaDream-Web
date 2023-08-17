d411f8697120151f02efe44d761ed5ae9faf2d91
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-08-18 04:08:29 +0900

    [FEAT] 봉사 정보 테그 여러개 가능

--- src/apis/Review/ScrapCheerSave.js
+++ src/apis/Review/ScrapCheerSave.js
@@ -16,17 +16,21 @@ export const FourMostCheered = async () => {
 
 export const SearchAreaKeyword = async (tagCode, areaCode) => { 
   if(tagCode==null){
-    console.log(tagCode);
+    console.log(tagCode)
     tagCode=undefined;
     
   }
   try{
-    const response = await axios.get(`${baseUrl}search/keyword/`, {
-      params: {
-        tagCode: tagCode ? `${tagCode}` : undefined,
-        areaCode: areaCode ? `${areaCode}` : undefined
-      }
-    });
+    console.log(areaCode)
+    const areaCodeArray = Array.isArray(areaCode) ? areaCode : [areaCode];
+    const areaCodeQuery = areaCodeArray.map(code => `areaCode=${code}`).join('&');
+
+    const tagCodeArray = Array.isArray(tagCode) ? tagCode : [tagCode];
+    const tagCodeQuery = tagCodeArray.map(code => `tagCodeQuery=${code}`).join('&');
+
+
+
+    const response = await axios.get(`${baseUrl}search/keyword/?${areaCodeQuery}?${tagCodeQuery}`);
     
     return response.data.data;
     
