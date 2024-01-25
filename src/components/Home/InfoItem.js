2eb95b2d47c00668f2ac818227e0964c95a6334d
kiiiiv <lizzy2@naver.com>
2024-01-25 14:46:20 +0900

    .

--- src/components/Home/InfoItem.js
+++ src/components/Home/InfoItem.js
@@ -13,8 +13,7 @@ const InfoItem = (props) => {
 
   return (
     <Infodiv width={width} height={height} onClick={onClick}>
-                <InfoImg style={                
-{                  backgroundImage: `url(${process.env.PUBLIC_URL}/tagImage/${url})`}                  }>
+                <InfoImg>
                     <Ddaydiv><div>D-{dday}</div></Ddaydiv>
                 </InfoImg>
                 <InfoTextDiv>
@@ -67,73 +66,50 @@ const InfoImg = styled.div`
   width : 100%;
   height : 55%;
   border-radius: 2em 2em 0 0;
-  
-  background-size: cover; /* 이미지를 컨테이너에 맞게 늘립니다 */
-  background-position: center; /* 이미지를 중앙으로 정렬합니다 */ 
 
-`
+`;
 
 const InfoTextDiv = styled.div`
-
-
   width: 100%;
   height : 45%;
-
   background: var(--secondary-yellow-color, #FFFAC9);
   box-sizing: border-box;
   padding: 0 1.2em;
-
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;  
   border-radius: 0 0 2em 2em;
-
-
-
 `
 
-
-//폰트 사이즈 2 4 2 축소 
-
 const InfoTextTag = styled.span`
-
   height : 17%;
-  
-
   color: var(--dark-color, #024959);
   font-size: 1.8em;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
   text-decoration-line: underline;
-
-  `
+`
 
 const InfoTextTitle = styled.span`
-
   height : 34%;
-
   overflow: hidden;
   color: #000;
   text-overflow: ellipsis;
-  whitespace: nowrap;
+  whiteSpace: nowrap;
   font-size: 1.8em;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-
 `
 
 const InfoTextDetail = styled.span`
-
   height : 17%;
-
   color: var(--dark-gray-color, #7E8181);
   text-align: center;
   font-size: 1.6em;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  
 `
